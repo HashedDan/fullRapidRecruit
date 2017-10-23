a24624e5104afa91689a134154ca8da85a8dc5de
HashedDan <dmangum@hashedhealth.com>
2017-10-23 15:40:01 -0500

    more routes and functions

--- server/request-handler.js
+++ server/request-handler.js
@@ -7,10 +7,49 @@ const connection = 'postgres://localhost:5432/g4';
 var client = new pg.Client(connection);
 client.connect();
 
+/*
+	GET FUNCTIONS
+*/
 exports.getOrganizations = (req, res) => {
   const results = [];
   const ticket = req.body;
-  // const query = client.query('INSERT INTO tickets(author, subject, issue, chatUrl, archive, status) values ($1, $2, $3, $4, $5, $6)', [ticket.author, ticket.subject, ticket.issue, ticket.chatUrl, ticket.archive, ticket.status]);
+  const query = client.query('SELECT * FROM organizations');
+  query.on('row', (row) => {
+    results.push(row);
+  })
+  query.on('end', () => {
+    return res.json(results);
+  });
+};
+
+exports.getMembers = (req, res) => {
+  const results = [];
+  const ticket = req.body;
+  const query = client.query('SELECT * FROM members');
+  query.on('row', (row) => {
+    results.push(row);
+  })
+  query.on('end', () => {
+    return res.json(results);
+  });
+};
+
+exports.getLists = (req, res) => {
+  const results = [];
+  const ticket = req.body;
+  const query = client.query('SELECT * FROM lists');
+  query.on('row', (row) => {
+    results.push(row);
+  })
+  query.on('end', () => {
+    return res.json(results);
+  });
+};
+
+exports.getEvents = (req, res) => {
+  const results = [];
+  const ticket = req.body;
+  const query = client.query('SELECT * FROM events');
   query.on('row', (row) => {
     results.push(row);
   })
--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
 const requestHandler = require('./request-handler.js');
 
+/*
+	GET ROUTES
+*/
 router.get('/organizations', requestHandler.getOrganizations);
+router.get('/members', requestHandler.getMembers);
+router.get('/lists', requestHandler.getLists);
+router.get('/events', requestHandler.getEvents);
 
 module.exports = router;
