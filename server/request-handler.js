7e977934493202ef9617fa2af304a66bc3ef6176
HashedDan <dmangum@hashedhealth.com>
2017-10-31 18:44:00 -0500

    all broad get routes complete

--- server/request-handler.js
+++ server/request-handler.js
@@ -8,7 +8,7 @@ var client = new pg.Client(connection);
 client.connect();
 
 /*
-	GET FUNCTIONS
+  ORGANIZATION ROUTES
 */
 exports.getOrganizations = (req, res) => {
   const query = client.query('SELECT * FROM organizations', (err, results) => {
@@ -16,20 +16,92 @@ exports.getOrganizations = (req, res) => {
   })
 };
 
+/*
+  MEMBERS ROUTES
+*/
 exports.getMembers = (req, res) => {
   const query = client.query('SELECT * FROM members', (err, results) => {
     return res.json(results.rows);
   })
 };
 
+/*
+  LISTS ROUTES
+*/
 exports.getLists = (req, res) => {
   const query = client.query('SELECT * FROM lists', (err, results) => {
     return res.json(results.rows);
   })
 };
 
+/*
+  EVENTS ROUTES
+*/
 exports.getEvents = (req, res) => {
   const query = client.query('SELECT * FROM events', (err, results) => {
+    return res.json(results.rows);
+  })
+};
+
+/*
+  RECRUIT ROUTES
+*/
+exports.getRecruits = (req, res) => {
+  const query = client.query('SELECT * FROM recruits', (err, results) => {
+    return res.json(results.rows);
+  })
+};
+
+/*
+  INTERACTIONS ROUTES
+*/
+exports.getInteractions = (req, res) => {
+  const query = client.query('SELECT * FROM interactions', (err, results) => {
+    return res.json(results.rows);
+  })
+};
+
+/*
+  INTERACTIONS_RECORDS ROUTES
+*/
+exports.getInteractionsRecords = (req, res) => {
+  const query = client.query('SELECT * FROM interactions_records', (err, results) => {
+    return res.json(results.rows);
+  })
+};
+
+/*
+  SIGN_IN ROUTES
+*/
+exports.getSignIn = (req, res) => {
+  const query = client.query('SELECT * FROM sign_in', (err, results) => {
+    return res.json(results.rows);
+  })
+};
+
+/*
+  VOTES ROUTES
+*/
+exports.getVotes = (req, res) => {
+  const query = client.query('SELECT * FROM votes', (err, results) => {
+    return res.json(results.rows);
+  })
+};
+
+/*
+  VOTES_RECORDS ROUTES
+*/
+exports.getVoteRecords = (req, res) => {
+  const query = client.query('SELECT * FROM votes_records', (err, results) => {
+    return res.json(results.rows);
+  })
+};
+
+/*
+  SHARE ROUTES
+*/
+exports.getShare = (req, res) => {
+  const query = client.query('SELECT * FROM share', (err, results) => {
     return res.json(results.rows);
   })
 };
--- server/routes.js
+++ server/routes.js
@@ -2,11 +2,58 @@ const router = require('express').Router();
 const requestHandler = require('./request-handler.js');
 
 /*
-	GET ROUTES
+	ORGANIZATION ROUTES
 */
 router.get('/organizations', requestHandler.getOrganizations);
+
+/*
+	MEMBERS ROUTES
+*/
 router.get('/members', requestHandler.getMembers);
+
+/*
+	LISTS ROUTES
+*/
 router.get('/lists', requestHandler.getLists);
+
+/*
+	EVENTS ROUTES
+*/
 router.get('/events', requestHandler.getEvents);
+
+/*
+	RECRUITS ROUTES
+*/
+router.get('/recruits', );
+
+/*
+	INTERACTIONS ROUTES
+*/
+router.get('/interactions', );
+
+/*
+	INTERACTIONS_RECORDS ROUTES
+*/
+router.get('/interactions_records', );
+
+/*
+	SIGN_IN ROUTES
+*/
+router.get('/sign_in', );
+
+/*
+	VOTES ROUTES
+*/
+router.get('/votes', );
+
+/*
+	VOTES_RECORDS ROUTES
+*/
+router.get('/vote_records', );
+
+/*
+	SHARE ROUTES
+*/
+router.get('/share', );
 
 module.exports = router;
