39e76906af09b5a3999605e7d3081c0b32db832f
Kenneth Breslow <breslow.k@wustl.edu>
2017-11-07 17:36:31 -0600

    more session stuff

--- client/my-org-view.component.js
+++ client/my-org-view.component.js
@@ -4,7 +4,7 @@ component('myOrgView', {
 	templateUrl: 'my-org-view.html',
 	controller: function myOrgController($routeParams, $scope, $http) {
 		//this needs to figure out what page you are on then pull the correct data to populate the cards
-
+	
 		$scope.urlHash = location.hash.toString();
 		$scope.view = $scope.urlHash.replace(/\#\!\//, '');
 
