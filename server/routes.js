0bc0c025298ba4f59f21e721908438dbce9b7e08
Kenneth Breslow <breslow.k@wustl.edu>
2017-11-07 10:51:26 -0600

    Commenting Unused Routes

--- server/routes.js
+++ server/routes.js
@@ -32,36 +32,36 @@ router.get('/events', requestHandler.getEvents);
 /*
 	RECRUITS ROUTES
 */
-router.get('/recruits', );
-
-/*
-	INTERACTIONS ROUTES
-*/
-router.get('/interactions', );
-
-/*
-	INTERACTIONS_RECORDS ROUTES
-*/
-router.get('/interactions_records', );
-
-/*
-	SIGN_IN ROUTES
-*/
-router.get('/sign_in', );
-
-/*
-	VOTES ROUTES
-*/
-router.get('/votes', );
-
-/*
-	VOTES_RECORDS ROUTES
-*/
-router.get('/vote_records', );
-
-/*
-	SHARE ROUTES
-*/
-router.get('/share', );
+//router.get('/recruits', );
+//
+///*
+//	INTERACTIONS ROUTES
+//*/
+//router.get('/interactions', );
+//
+///*
+//	INTERACTIONS_RECORDS ROUTES
+//*/
+//router.get('/interactions_records', );
+//
+///*
+//	SIGN_IN ROUTES
+//*/
+//router.get('/sign_in', );
+//
+///*
+//	VOTES ROUTES
+//*/
+//router.get('/votes', );
+//
+///*
+//	VOTES_RECORDS ROUTES
+//*/
+//router.get('/vote_records', );
+//
+///*
+//	SHARE ROUTES
+//*/
+//router.get('/share', );
 
 module.exports = router;
