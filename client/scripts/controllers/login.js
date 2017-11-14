dfac2dec25e88cca7ff7e2941e09e44bd59085d5
Kenneth Breslow <breslow.k@wustl.edu>
2017-11-13 22:40:22 -0600

    some stuff

--- client/scripts/controllers/login.js
+++ client/scripts/controllers/login.js
@@ -8,9 +8,9 @@
  * Controller of yapp
  */
 angular.module('yapp')
-  .controller('LoginCtrl', function($scope, $http, $location) {
+	.controller('LoginCtrl', function($scope, $http, $location) {
 
-    	//$scope.responseMessage = "No Response Message.";
+		//$scope.responseMessage = "No Response Message.";
 
 		$scope.createPost = function(data) {
 
@@ -29,27 +29,27 @@ angular.module('yapp')
 				})
 				.then(function(response) {
 					//code to execute on success
-                    //$scope.responseMessage = "Status: " + response.status + " | Status Text: " + response.statusText;
-                    $location.path('/dashboard');
+					//$scope.responseMessage = "Status: " + response.status + " | Status Text: " + response.statusText;
+					$location.path('/dashboard');
 				})
 				.catch(function(err) {
 					//code to execute on error
 					//$scope.responseMessage = "Status: " + err.status + " | Status Text: " + err.statusText;
 				});
 		};
-        
-        $scope.createPostRegister = function(data) {
-            
+
+		$scope.createPostRegister = function(data) {
+
 			var dataObj = {};
 
 			dataObj.email = data.regemail;
 			dataObj.password = data.regpassword;
-            dataObj.first = data.regfirst;
-            dataObj.last = data.reglast;
-            dataObj.org = data.regorg;
-            //dataObj.level = data.reglevel;
-            
-            //$scope.responseMessage = dataObj;
+			dataObj.first = data.regfirst;
+			dataObj.last = data.reglast;
+			dataObj.org = data.regorg;
+			//dataObj.level = data.reglevel;
+
+			//$scope.responseMessage = dataObj;
 
 			$http({
 					method: 'POST',
@@ -61,12 +61,12 @@ angular.module('yapp')
 				})
 				.then(function(response) {
 					//code to execute on success
-                    //$scope.responseMessage = "Status: " + response.status + " | Status Text: " + response.statusText;
-                    $location.path('/dashboard');
+					//$scope.responseMessage = "Status: " + response.status + " | Status Text: " + response.statusText;
+					$location.path('/dashboard');
 				})
 				.catch(function(err) {
 					//code to execute on error
 					//$scope.responseMessage = "Status: " + err.status + " | Status Text: " + err.statusText;
 				});
 		};
-  });
+	});
